cfb4db83bbc452bb4aa4528abc6956f645c2e602
etdeg16 <ethandguzman@gmail.com>
2017-02-06 15:47:09 -0800

    Removed code from EE 151 challenge

--- RobotDrive/RobotDrive.ino
+++ RobotDrive/RobotDrive.ino
@@ -8,7 +8,6 @@ const int ENA = 5; // ENA (PWM or Enable for Motor A)
 const int ENB = 6; // ENB (PWM or Enable for Motor B)
 const int servoPin = 11; //Servo that controls the ultrasonic sensor
 const int eggServoPin = 3;
-const int tonePin = 2; //Controls speakers
 const int sensor1 = A0; //Line-following sensors
 const int sensor2 = A1;
 const int sensor3 = A2;
@@ -17,21 +16,13 @@ int state2;
 int state3;
 boolean withinDistance; //Checks if the robot is within a certain distance from an obstacle
 int rememberLastPosition; //Remembers whether the robot was to the left of the black line, to the right of the black line, or on the black line
-float cupDistance; //Calculates the distance from the ultrasonic sensor to the cup
-boolean ledOn = false;
 boolean obstaclePassed = false;
-
 const int rangeTriggerPin = A5;
 const int rangeSensorPin = 4;
 const unsigned long rangeTimeout = 2000;
 unsigned long echoDelay = 0;
 Servo servo;
-Servo eggServo;
 
-const char notes[] = "gabygabyxzCDxzCDabywabywzCDEzCDEbywFCDEqywFGDEqi        azbC"; // a space represents a rest, letters represent notes
-int length = sizeof(notes); // the number of notes
-int beats[] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 16,};
-int tempo = 100;
 
 void setup() {
   pinMode(motorA1, OUTPUT); //set pins on motors as outputs
@@ -40,17 +31,14 @@ void setup() {
   pinMode(motorB1, OUTPUT);
   pinMode(motorB2, OUTPUT);
   pinMode(ENB, OUTPUT);
-  pinMode(tonePin, OUTPUT); //set pin of speaker as output
   pinMode(rangeTriggerPin, OUTPUT);
   pinMode(rangeSensorPin, INPUT);
   pinMode(sensor1, INPUT); //set pins of line sensors as inputs
   pinMode(sensor2, INPUT);
   pinMode(sensor3, INPUT);
-  pinMode(13, OUTPUT); //set LED pin on board as output
   servo.attach(servoPin); //intialize servos
-  eggServo.attach(eggServoPin);
   servo.write(75); //move servo on sensor to face forward
-  eggServo.write(180); //move servo on egg to hold the egg
+
 }
 
 void loop() {
@@ -59,10 +47,6 @@ void loop() {
   state2 = digitalRead(sensor2);
   state3 = digitalRead(sensor3);
   withinDistance = obstacleDistance(10.0) > 2.0 && obstacleDistance(10.0) <= 10.0; //check if an obstacle is detected between 2 and 10 inches
-  if (obstaclePassed){ 
-    noTone(tonePin); //shuts off tone that indicates an obstacle is detected
-    obstaclePassed = false;
-  }
   if (state2 == HIGH) { //middle sensor detects black
     if (state1 == HIGH && state3 == LOW) { //robot is positioned slightly off to the right
       rememberLastPosition = 3; //remember that robot was off to the right
@@ -103,51 +87,17 @@ void loop() {
   }
   if (withinDistance) { //obstacle is detected 8 inches away
     stop();
-    noTone(tonePin); //turn off tone of robot moving
-    if (state1 == HIGH && state2 == HIGH && state3 == HIGH && obstacleDetected(8.0)) { //line sensors detect all black and an obstacle is detected within 8 inches away
-      stop();
-      noTone(tonePin);
-      delay(1000);
-      cupDistance = obstacleDistance(10.0); //check distance from sensor to cup
-      while (cupDistance < 4.5 || cupDistance > 4.9) { //egg dropping mechanism is not aligned with the cup
-        if (cupDistance < 4.5) { //robot is too close to the cup
-          moveBackward(128, 10);
-        }
-        else if (cupDistance > 4.9) { //robot is too far away from the cup
-          moveForward(128, 10);
-        }
-        cupDistance = obstacleDistance(10.0); //check distance from robot to cup again
-      }
-      stop();
-      delay(1000);
-      eggServo.write(90); //turn egg servo to 90 degree position to allow egg to follow
-      playSong(); //play song to show that egg is ready
-      while(true){ //stop the robot for an infinite amount of time
-      stop();
-      }
-    }
-    else {
+  }
+   else {
       while (withinDistance) { //robot will still stop as long as an obstacle is detected
-        tone(tonePin, map((echoDelay / 148.148), 0, 30, 60, 960)); //play tone to indicate obstacle is detected
-        delayMicroseconds(10);
         delay(1500);
         withinDistance = obstacleDistance(10.0) > 2.0 && obstacleDistance(10.0) <= 10.0; //check if robot still detects an obstacle
       }
-      obstaclePassed = true; //will allow the obstacle detection tone to turn off once the loop starts
     }
   }
-  if (ledOn){
-    digitalWrite(13, HIGH); //flash LED on and turn tone on
-    tone(tonePin, 440);
-  } 
-  else{
-    digitalWrite(13, LOW); //turn LED off and shut off tone
-    noTone(tonePin);
-  }
-  ledOn = !ledOn; //allows LED to flash on and off
 
 
-}
+
 
 void moveForward(int speed, int time) { //moves robot forward by powering both the left and right motors forward at equal speeds
   analogWrite(ENA, speed); //set speed of motor
@@ -239,47 +189,9 @@ boolean obstacleDetected(float withinInches) { //uses ultrssonic sensor to detec
   digitalWrite(rangeTriggerPin, LOW);
   echoDelay = pulseIn(rangeSensorPin, HIGH, rangeTimeout); //measure time it takes to send signal and retrieve it
   float length = echoDelay / 148.148;
-  if (echoDelay == 0) { 
-    noTone(2);
-    return false;
-  }
-  else { 
-    analogWrite(2, HIGH);
-
-    return length <= withinInches;
-  }
-}
+  return length <= withinInches;
+ }
 
 
 
-void playTone(int tone, int duration) {
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(tonePin, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(tonePin, LOW);
-    delayMicroseconds(tone);
-  }
-}
 
-void playNote(char note, int duration) {
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'x', 'a', 'z', 'b', 'C', 'y', 'D', 'w', 'E', 'F', 'q', 'G', 'i' };
-  // c=C4, C = C5. These values have been tuned.
-  int tones[] = { 1898, 1690, 1500, 1420, 1265, 1194, 1126, 1063, 1001, 947, 893, 843, 795, 749, 710, 668, 630, 594 }; //match notes with frequencies
-
-  // play the tone corresponding to the note name
-  for (int i = 0; i < 18; i++) {
-    if (names[i] == note) {
-      playTone(tones[i], duration);
-    }
-  }
-}
-
-void playSong() {
-   for (int i = 0; i < length; i++) { //play through array of notes
-    if (notes[i] == ' ') {
-      delay(beats[i] * tempo); // rest
-    } else {
-      playNote(notes[i], beats[i] * tempo);
-    }
-   }
-}
